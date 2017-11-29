8e55c97aeab1384b35b825cd0fa9109a2517f27e
edward <bauchiroad@gmail.com>
2017-11-29 22:36:15 +0000

    changed ember-data model to use a custom transform for date

--- frontend/app/models/event.js
+++ frontend/app/models/event.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr('string'),
-  start: DS.attr('date'),
-  finish: DS.attr('date'),
+  start: DS.attr('moment-date'),
+  finish: DS.attr('moment-date'),
   title: DS.attr('string')
 });
---
+++ frontend/app/transforms/moment-date.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+
+export default DS.Transform.extend({
+  deserialize(serialized) {
+    if (serialized) {
+      return moment(serialized);
+    }
+
+    return serialized;
+  },
+
+  serialize(deserialized) {
+    if (deserialized) {
+      return moment(deserialized).toDate();
+    }
+    return deserialized;
+  }
+});
